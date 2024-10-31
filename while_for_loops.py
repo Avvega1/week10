c44640740673d62ad8f7bf017be58ab9b608efb7
Vega <avvega1@cps.k12.il.us>
2024-10-31 15:08:35 -0500

    first commit

---
+++ while_for_loops.py
@@ -0,0 +1,43 @@
+# while loop = execute some code WHILE some condition remains true
+# while loops and for loops
+# are forms of iteration
+# iteration is the process of
+# repeating or looping through
+# a set of steps or instructions
+# to iterate is the verb
+# form of iteration
+# be careful of infinite loops
+# they will crash your program
+# and you will have to restart it
+# infinite loops are loops that never end
+
+# name = input("Enter your name: ")
+
+# while name == "":
+#     print(" You did not enter your name")
+
+#     print(f"hello{name}")
+
+
+# age = int(input("Enter your age: "))
+
+# while age < 0 :
+#     print(" Age cant be negative")
+# age = int(input("Enter your age :"))
+
+# print(f"you are {age} years old")
+
+# food = input("Enter a food you like ( q to quit): ")
+
+# while not food == "a":
+#     print(f"you like {food}")
+#     food = input ("Enter another food you like (q to quit): ")
+
+#     print("bye")
+
+
+# num = int(input("Enter a # between 1 - 10 :"))
+# while num < 1 or num > 10 :
+#     print(f"{num} is not valid")
+#     num = int(input("Enter a # between 1 - 10 :"))
+#     print(f"your number is {num}")
---
+++ while_for_loops2.py
@@ -0,0 +1,51 @@
+# for loops = execute a block of code a fixed number of times. 
+#             you can iterate over range, string, sequence, etc.
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+
+
+# print("HAPPY NEW YEAR")
+
+# for x in range(1, 11, 3):
+#     print(x)
+
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card:
+#     print(x)
+
+# for x in range(1, 21):
+#     if x == 13 or x == 15 or x == 19:
+#         break #skips over a number - (continue)
+#     else:
+#         print(x)
+
+
+####Challenge-1####
+# horror_movies = ['Exorcist', 'The Shining', 'The Conjuring' , 'The Ring']
+
+# for movie in horror_movies:
+#     if movie == 'The Shining':
+#         print('The Shining is found')
+#     else :
+#         print('The Shining was not found')
+#         print(movie)
+
+
+
+
+
+####Challenge-2####
+# horror_movie_characters = ['Art The Clown', 'Jason', 'Michael Myers', 'Pennywise', 'Freddy Krueger', 'Freddy Fazbear','Mickey Mouse']
+# for x in horror_movie_characters:
+#     if x == 'Pennywise':
+#         continue
+#     else:
+#         print(x)
+
+
+
+
+####Challenge-3####
